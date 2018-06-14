16dcecd82ee3f7f39ef25174ac1d88e92bf38f1b
memmedlisakit <memmedlisakit@gmail.com>
2018-06-14 23:33:38 +0400

    14_06_2018 lab

--- js/main.js
+++ js/main.js
@@ -4,34 +4,76 @@
 
 
 $(document).ready(function () {
+    // let div = $(".div"); 
+    // // function test() {
+    // //     alert("lxjfhgfdk")
+    // // } 
+    // $("p").click(function () {
+    //     $(div).hide(3000, function () {
 
-    let div = $(".div");
+    //     });
+    // }); 
+    // $(".btn_custom").on("mouseover", function () {
+    //     let id = $(this).data("card-id")
+    //     console.log(id)
+    //     $("#"+id).slideUp(2000, function(){
+    //         $("#"+id).slideDown(2000);
+    //     }); 
+    //     // let jsid = this.dataset.card;
+    //     // console.log(jsid);
+    // }) 
 
-    // function test() {
-    //     alert("lxjfhgfdk")
-    // }
+    $(".btn_position").on("click", function () {
+        // let styles = {
+        //     width: "500px",
+        //     height: "200px",
+        //     background: "red",
+        // }
 
-    $("p").click(function () {
-        $(div).hide(3000, function () {
+        // $(".green").css(styles);
+        // $(".green").animate({
+        //     top: "+=150px"
+        // }, 1000, function(){
+        //     $(".green").animate({
+        //         top:"-=150px"
+        //     }, 1000)
+        // }).css({
+        //     "fontSize":"55px"
+        // }).addClass("div")
 
-        });
-    });
 
-    $(".btn_custom").on("mouseover", function () {
-        let id = $(this).data("card-id")
-        console.log(id)
-        $("#"+id).hide(2000, function(){
-            $("#"+id).show(2000);
-        });
 
-        // let jsid = this.dataset.card;
-        // console.log(jsid);
+        // GET text
+        // let text01 = $(".green").html();
+        // let text02 = $(".green").text();
+        // console.log(text01);
+
+
+
+        //  SET text
+        // $(".green").html("<h1>hello world</h1>")
+        // $(".green").text("<h1>hello world</h1>")
+
+
+
+
+        $(".green").prepend("<h1>prepend header</h1>")
+        $(".green").append("<h1>append footer</h1>")
+
+
+
+        $(".text").before("<i>before italic text</i>") 
+        $(".text").after("<i>after italic text</i>")
+
+
+        let width = $(".green").width();
+        console.log(width);
+
+        $(".green").toggleClass("div")
+
+
     })
-
-
-
-
-
-
 })
+
+
 
